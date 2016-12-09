58bf69b886f3f5ed7036c77063e848d145d59a30
Jaewon Seo <byeol@hotmail.co.kr>
2016-12-09 12:32:53 +0900

    Integration

--- src/main/java/org/nhnnext/repository/DiscussionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.nhnnext.repository;
-
-import org.nhnnext.domain.Discussion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DiscussionRepository extends CrudRepository<Discussion, Long> {
-}
