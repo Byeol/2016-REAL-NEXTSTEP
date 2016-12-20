10da77e4a7875473ad7a8742ad955b6412d698f4
Jaewon Seo <byeol@hotmail.co.kr>
2016-12-20 20:22:31 +0900

    Update

--- src/main/java/org/nhnnext/nextstep/config/HttpSessionConfig.java
+++ src/main/java/org/nhnnext/nextstep/config/HttpSessionConfig.java
@@ -1,16 +1,24 @@
 package org.nhnnext.nextstep.config;
 
-//@Configuration
-//@EnableRedisHttpSession
-//public class HttpSessionConfig {
-//
-//    @Bean
-//    public LettuceConnectionFactory connectionFactory() {
-//        return new LettuceConnectionFactory();
-//    }
-//
-//    @Bean
-//    public HttpSessionStrategy httpSessionStrategy() {
-//        return new HeaderHttpSessionStrategy();
-//    }
-//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+@Configuration
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
+}
